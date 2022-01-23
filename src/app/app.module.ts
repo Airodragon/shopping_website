a56353aa576899f5e94c1376026526efef47653f
Airodragon <airodragon1919@gmail.com>
2022-01-23 15:45:56 +0530

    navadded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './MyComponents/navbar/navbar.component';
-import { ListOrderComponent } from './MyComponents/list-order/list-order.component';
-import { OrdersModule } from './modules/orders/orders.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ListOrderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    OrdersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
