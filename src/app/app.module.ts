c2d0f11fabfbd8e5bac7cdc61c953cca89e01b66
Airodragon <airodragon1919@gmail.com>
2022-01-23 15:38:59 +0530

    some changesz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './MyComponents/navbar/navbar.component';
+import { ListOrderComponent } from './MyComponents/list-order/list-order.component';
+import { OrdersModule } from './modules/orders/orders.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListOrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OrdersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
