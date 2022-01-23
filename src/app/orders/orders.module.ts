80c37f9d31833dd02f80aa12e9b1a300e1855e20
Airodragon <airodragon1919@gmail.com>
2022-01-23 19:13:17 +0530

    ordersadded

---
+++ src/app/orders/orders.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListOrderComponent } from './list-order/list-order.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListOrderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class OrdersModule { }
